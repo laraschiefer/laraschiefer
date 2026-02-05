c53a80813d41c45f0559ae76e3fe24ed1f0e6513
laraschiefer <laraschiefer1@gmail.com>
2026-02-05 13:22:38 +0100

    bowbackground

--- src/modules/render/content.js
+++ src/modules/render/content.js
@@ -5,5 +5,6 @@ export const content = [
   { file: './content/fun/IMG_0407.jpg', lastModified: '2026-01-28T13:08:59Z', fileType: 'IMAGE' },
   { file: './content/fun/IMG_0408.jpg', lastModified: '2026-01-28T13:08:59Z', fileType: 'IMAGE' },
   { file: './content/fun/testt.txt', lastModified: '2026-01-28T13:08:59Z', fileType: 'TXT' },
+  { file: './content/video/.DS_Store', lastModified: '2026-02-05T12:58:41Z', fileType: 'null' },
   { file: './content/video/Bliss.mov', lastModified: '2026-02-05T12:58:41Z', fileType: 'VIDEO' },
 ];
